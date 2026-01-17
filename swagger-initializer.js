eaa2a32bdbd88f772ca2461b9f25ed5247621dba
ba2slk <130782318+ba2slk@users.noreply.github.com>
2026-01-17 12:40:57 +0900

    Update Swagger UI YAML URL to TeamCatchUp repo

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/CEOS-22nd-CatchUP/swagger-ui/refs/heads/master/swagger.yaml",
+    url: "https://raw.githubusercontent.com/TeamCatchUp/swagger-ui/refs/heads/master/swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
